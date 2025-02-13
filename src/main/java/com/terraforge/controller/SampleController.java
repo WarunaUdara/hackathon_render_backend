1b2b85134aba4b6ed6220d8cfd8653bc33208326
Uhanovitage Waruna Udara Sampath <warunaudarasam2003@gmail.com>
2025-02-14 03:29:36 +0530

    loggin enabled

--- src/main/java/com/terraforge/controller/SampleController.java
+++ src/main/java/com/terraforge/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.terraforge.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
+@Slf4j
 public class SampleController {
     @RequestMapping("/sample")
     public String sample() {
+        log.info("SampleController.sample() called");
         return "Hello, World!";
     }
 }
